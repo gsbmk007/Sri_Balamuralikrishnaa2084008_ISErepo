1c1885f630cac51514653a8f9a09d898739fc029
gsbmk007 <gsbmkrishnaa@gmail.com>
2023-06-02 23:41:22 +0800

    ui working , greu bg test

--- SeasonFinder.java
+++ SeasonFinder.java
@@ -6,6 +6,10 @@ import java.util.Map;
 import java.util.Scanner;
 import javax.swing.*;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
 public class SeasonFinder {
     public static void main(String[] args) {
 
@@ -17,44 +21,45 @@ public class SeasonFinder {
         JFrame frame = new JFrame("Season Finder");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
-        frame.setLayout(null);
+
+        // Set the background color of the frame to dark grey (bg-gray-800)
+        frame.getContentPane().setBackground(new Color(51, 51, 51));
+        frame.getContentPane().setLayout(new BorderLayout());
+
+        // Create the panel for the form
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 2, 10, 10));
+        formPanel.setBackground(new Color(51, 51, 51));
 
         // Create the UI components
         JLabel countryLabel = new JLabel("Country:");
-        countryLabel.setBounds(20, 20, 120, 25);
-        frame.add(countryLabel);
-
         JTextField countryTextField = new JTextField();
-        countryTextField.setBounds(150, 20, 200, 25);
-        frame.add(countryTextField);
 
-        JLabel monthLabel = new JLabel("Month 12");
-        monthLabel.setBounds(20, 50, 200, 25);
-        frame.add(monthLabel);
-
+        JLabel monthLabel = new JLabel("Month (1-12):");
         JTextField monthTextField = new JTextField();
-        monthTextField.setBounds(230, 50, 120, 25);
-        frame.add(monthTextField);
 
         JLabel cityLabel = new JLabel("City name:");
-        cityLabel.setBounds(230, 80, 120, 25);
-        frame.add(cityLabel);
-
         JTextField cityTextField = new JTextField();
-        cityTextField.setBounds(150, 80, 200, 25);
-        frame.add(cityTextField);
 
         JLabel temperatureLabel = new JLabel("Temperature reading:");
-        temperatureLabel.setBounds(20, 110, 200, 25);
-        frame.add(temperatureLabel);
-
         JTextField temperatureTextField = new JTextField();
-        temperatureTextField.setBounds(30, 110, 120, 25);
-        frame.add(temperatureTextField);
 
+        countryLabel.setForeground(Color.WHITE);
+        monthLabel.setForeground(Color.WHITE);
+        cityLabel.setForeground(Color.WHITE);
+        temperatureLabel.setForeground(Color.WHITE);
+
+        formPanel.add(countryLabel);
+        formPanel.add(countryTextField);
+        formPanel.add(monthLabel);
+        formPanel.add(monthTextField);
+        formPanel.add(cityLabel);
+        formPanel.add(cityTextField);
+        formPanel.add(temperatureLabel);
+        formPanel.add(temperatureTextField);
+
+        // Create the submit button
         JButton submitButton = new JButton("Submit");
-        submitButton.setBounds(150, 140, 100, 25);
-        frame.add(submitButton);
 
         // Add action listener to the submit button
         submitButton.addActionListener(e -> {
@@ -73,6 +78,18 @@ public class SeasonFinder {
                 JOptionPane.showMessageDialog(frame, "Season data not found for the given country and month.");
             }
         });
+
+        // Create a panel for the submit button
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(51, 51, 51));
+        buttonPanel.add(submitButton);
+
+        // Add the form panel and button panel to the frame
+        frame.add(formPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        // Make the frame resizable
+        frame.setResizable(true);
 
         // Make the frame visible
         frame.setVisible(true);
