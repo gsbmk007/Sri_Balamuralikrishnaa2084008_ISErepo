937c3e1e28a6f34bc94b72e98f5711aaa50d123a
gsbmk007 <gsbmkrishnaa@gmail.com>
2023-06-02 15:32:46 +0800

    First commit

---
+++ io.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Untitled {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Country: ");
+        
+
+
+	}
+}
