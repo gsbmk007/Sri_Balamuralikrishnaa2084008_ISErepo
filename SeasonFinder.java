3549d1c204f2b855df42c39346605a5694dd27b8
gsbmk007 <gsbmkrishnaa@gmail.com>
2023-06-02 17:21:05 +0800

    Merged seperatrly working temp and season finders

--- SeasonFinder.java
+++ SeasonFinder.java
@@ -6,46 +6,39 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class SeasonFinder {
-    // public static void main(String[] args) {
-
-    //     // Read the seasons data from the file
-    //     Map<String, String[]> seasonsData =seasonhandler.readSeasonsDataFromFile("Seasons.csv");
-
-    //     // Prompt the user for input
-    //     Scanner scanner = new Scanner(System.in);
-    //     System.out.print("Enter the country: ");
-    //     String country = scanner.nextLine();
-    //     System.out.print("Enter the month number (1-12): ");
-    //     int month = scanner.nextInt();
-
-    //     // Find and display the corresponding season
-    //     String season = seasonhandler.getSeason(seasonsData, country, month);
-    //     if (season != null) {
-    //         System.out.println("The season in " + country + " for month " + month + " is: " + season);
-    //     } else {
-    //         System.out.println("Season data not found for the given country and month.");
-    //     }
-    // }
-
     public static void main(String[] args) {
-        String filename = "temps.csv";
-        TemperatureHandler.readTemperatureDataFromFile(filename);
 
+        // Read the seasons data from the file
+        Map<String, String[]> seasonsData =seasonhandler.readSeasonsDataFromFile("Seasons.csv");
+        TemperatureHandler.readTemperatureDataFromFile("temps.csv");
+
+        // Prompt the user for input
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the country name: ");
+        System.out.print("Enter the country: ");
         String country = scanner.nextLine();
-
+        System.out.print("Enter the month number (1-12): ");
+        int month = scanner.nextInt();
         System.out.print("Enter the city name: ");
         String city = scanner.nextLine();
 
+         city = scanner.nextLine();
+
+        
         System.out.print("Enter the temperature reading: ");
         double temperature = scanner.nextDouble();
+        
 
-        scanner.close();
-
+        // Find and display the corresponding season
+        String season = seasonhandler.getSeason(seasonsData, country, month);
         TemperatureHandler.compareTemperature(country, city, temperature);
+
+        if (season != null) {
+            System.out.println("The season in " + country + " for month " + month + " is: " + season);
+        } else {
+            System.out.println("Season data not found for the given country and month.");
+        }
     }
+
 
 
     
