47c260071f77444f675d0c4ae6fc6211071f26ac
gsbmk007 <gsbmkrishnaa@gmail.com>
2023-06-02 23:30:07 +0800

    Added ui for the program

--- SeasonFinder.java
+++ SeasonFinder.java
@@ -4,42 +4,77 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class SeasonFinder {
     public static void main(String[] args) {
 
         // Read the seasons data from the file
-        Map<String, String[]> seasonsData =seasonhandler.readSeasonsDataFromFile("Seasons.csv");
+        Map<String, String[]> seasonsData = seasonhandler.readSeasonsDataFromFile("Seasons.csv");
         TemperatureHandler.readTemperatureDataFromFile("temps.csv");
 
-        // Prompt the user for input
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the country: ");
-        String country = scanner.nextLine();
-        System.out.print("Enter the month number (1-12): ");
-        int month = scanner.nextInt();
-        System.out.print("Enter the city name: ");
-        String city = scanner.nextLine();
+        // Create the main frame
+        JFrame frame = new JFrame("Season Finder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLayout(null);
 
-         city = scanner.nextLine();
+        // Create the UI components
+        JLabel countryLabel = new JLabel("Country:");
+        countryLabel.setBounds(20, 20, 120, 25);
+        frame.add(countryLabel);
 
-        
-        System.out.print("Enter the temperature reading: ");
-        double temperature = scanner.nextDouble();
-        
+        JTextField countryTextField = new JTextField();
+        countryTextField.setBounds(150, 20, 200, 25);
+        frame.add(countryTextField);
 
-        // Find and display the corresponding season
-        String season = seasonhandler.getSeason(seasonsData, country, month);
-        TemperatureHandler.compareTemperature(country, city, temperature);
+        JLabel monthLabel = new JLabel("Month 12");
+        monthLabel.setBounds(20, 50, 200, 25);
+        frame.add(monthLabel);
 
-        if (season != null) {
-            System.out.println("The season in " + country + " for month " + month + " is: " + season);
-        } else {
-            System.out.println("Season data not found for the given country and month.");
-        }
+        JTextField monthTextField = new JTextField();
+        monthTextField.setBounds(230, 50, 120, 25);
+        frame.add(monthTextField);
+
+        JLabel cityLabel = new JLabel("City name:");
+        cityLabel.setBounds(230, 80, 120, 25);
+        frame.add(cityLabel);
+
+        JTextField cityTextField = new JTextField();
+        cityTextField.setBounds(150, 80, 200, 25);
+        frame.add(cityTextField);
+
+        JLabel temperatureLabel = new JLabel("Temperature reading:");
+        temperatureLabel.setBounds(20, 110, 200, 25);
+        frame.add(temperatureLabel);
+
+        JTextField temperatureTextField = new JTextField();
+        temperatureTextField.setBounds(30, 110, 120, 25);
+        frame.add(temperatureTextField);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(150, 140, 100, 25);
+        frame.add(submitButton);
+
+        // Add action listener to the submit button
+        submitButton.addActionListener(e -> {
+            String country = countryTextField.getText();
+            int month = Integer.parseInt(monthTextField.getText());
+            String city = cityTextField.getText();
+            double temperature = Double.parseDouble(temperatureTextField.getText());
+
+            // Find and display the corresponding season
+            String season = seasonhandler.getSeason(seasonsData, country, month);
+            TemperatureHandler.compareTemperature(country, city, temperature);
+
+            if (season != null) {
+                JOptionPane.showMessageDialog(frame, "The season in " + country + " for month " + month + " is: " + season);
+            } else {
+                JOptionPane.showMessageDialog(frame, "Season data not found for the given country and month.");
+            }
+        });
+
+        // Make the frame visible
+        frame.setVisible(true);
     }
-
-
-
-    
 }
