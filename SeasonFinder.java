f2944b9f307204d1577104acf596691b09c088b6
gsbmk007 <gsbmkrishnaa@gmail.com>
2023-06-03 15:43:58 +0800

    made changes to the temperature handler

--- SeasonFinder.java
+++ SeasonFinder.java
@@ -7,124 +7,122 @@ import javax.swing.border.Border;
 import java.util.Map;
 
 public class SeasonFinder {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    // Read the seasons data from the file
-    Map<String, String[]> seasonsData = SeasonHandler.readSeasonsDataFromFile("Seasons.csv");
-    TemperatureHandler.readTemperatureDataFromFile("temps.csv");
+        // Read the seasons data from the file
+        Map<String, String[]> seasonsData = SeasonHandler.readSeasonsDataFromFile("Seasons.csv");
+        TemperatureHandler.readTemperatureDataFromFile("temps.csv");
 
-    // Create the main frame
-    JFrame frame = new JFrame("Season Finder");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(400, 300);
+        // Create the main frame
+        JFrame frame = new JFrame("Season Finder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
 
-    // Set the background color of the frame to dark grey (bg-gray-800)
-    frame.getContentPane().setBackground(new Color(51, 51, 51));
-    frame.getContentPane().setLayout(new BorderLayout());
+        // Set the background color of the frame to dark grey (bg-gray-800)
+        frame.getContentPane().setBackground(new Color(51, 51, 51));
+        frame.getContentPane().setLayout(new BorderLayout());
 
-    // Create the panel for the form
-    JPanel formPanel = new JPanel();
-    formPanel.setLayout(new GridLayout(7, 2, 10, 10));
-    formPanel.setBackground(new Color(51, 51, 51));
+        // Create the panel for the form
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(7, 2, 10, 10));
+        formPanel.setBackground(new Color(51, 51, 51));
 
-    // Create a line border with grey color
-    Border border = BorderFactory.createLineBorder(Color.GRAY);
+        // Create a line border with grey color
+        Border border = BorderFactory.createLineBorder(Color.GRAY);
 
-    // Create the UI components
-    JLabel countryLabel = new JLabel("Country:");
-    countryLabel.setForeground(Color.WHITE);
-    JTextField countryTextField = new JTextField();
-    countryTextField.setBorder(border);
+        // Create the UI components
+        JLabel countryLabel = new JLabel("Country:");
+        countryLabel.setForeground(Color.WHITE);
+        JTextField countryTextField = new JTextField();
+        countryTextField.setBorder(border);
 
-    JLabel monthLabel = new JLabel("Month (1-12):");
-    monthLabel.setForeground(Color.WHITE);
-    JTextField monthTextField = new JTextField();
-    monthTextField.setBorder(border);
+        JLabel monthLabel = new JLabel("Month (1-12):");
+        monthLabel.setForeground(Color.WHITE);
+        JTextField monthTextField = new JTextField();
+        monthTextField.setBorder(border);
 
-    JLabel cityLabel = new JLabel("City name:");
-    cityLabel.setForeground(Color.WHITE);
+        JLabel cityLabel = new JLabel("City name:");
+        cityLabel.setForeground(Color.WHITE);
+        JTextField cityTextField = new JTextField();
+        cityTextField.setBorder(border);
 
-    JTextField cityTextField = new JTextField();
-    cityTextField.setBorder(border);
+        JLabel timeOfDayLabel = new JLabel("Time of Day (Morning/Evening):");
+        timeOfDayLabel.setForeground(Color.WHITE);
+        JTextField timeOfDayTextField = new JTextField();
+        timeOfDayTextField.setBorder(border);
 
-    JLabel timeOfDayLabel = new JLabel("Time of Day (Morning/Evening):");
-    timeOfDayLabel.setForeground(Color.WHITE);
-    JTextField timeOfDayTextField = new JTextField();
-    timeOfDayTextField.setBorder(border);
+        JLabel temperatureLabel = new JLabel("Temperature reading:");
+        JTextField temperatureTextField = new JTextField();
+        temperatureTextField.setBorder(border);
+        temperatureLabel.setForeground(Color.WHITE);
 
-    JLabel temperatureLabel = new JLabel("Temperature reading:");
-    JTextField temperatureTextField = new JTextField();
-    temperatureTextField.setBorder(border);
-    temperatureLabel.setForeground(Color.WHITE);
+        JLabel resultLabel = new JLabel("Season:");
+        resultLabel.setForeground(Color.WHITE);
+        JLabel seasonResultLabel = new JLabel("");
+        seasonResultLabel.setForeground(Color.WHITE);
 
-    JLabel resultLabel = new JLabel("Season:");
-    resultLabel.setForeground(Color.WHITE);
-    JLabel seasonResultLabel = new JLabel("");
-    seasonResultLabel.setForeground(Color.WHITE);
+        JLabel resultTextLabel = new JLabel("Result:");
+        resultTextLabel.setForeground(Color.WHITE);
+        JTextArea resultTextArea = new JTextArea();
+        resultTextArea.setEditable(false);
+        resultTextArea.setForeground(Color.WHITE);
+        resultTextArea.setBackground(new Color(51, 51, 51));
+        JScrollPane scrollPane = new JScrollPane(resultTextArea);
 
-    JLabel resultTextLabel = new JLabel("Result:");
-    resultTextLabel.setForeground(Color.WHITE);
-    JTextArea resultTextArea = new JTextArea();
-    resultTextArea.setEditable(false);
-    resultTextArea.setForeground(Color.WHITE);
-    resultTextArea.setBackground(new Color(51, 51, 51));
-    JScrollPane scrollPane = new JScrollPane(resultTextArea);
+        formPanel.add(countryLabel);
+        formPanel.add(countryTextField);
+        formPanel.add(monthLabel);
+        formPanel.add(monthTextField);
+        formPanel.add(cityLabel);
+        formPanel.add(cityTextField);
+        formPanel.add(timeOfDayLabel);
+        formPanel.add(timeOfDayTextField);
+        formPanel.add(temperatureLabel);
+        formPanel.add(temperatureTextField);
+        formPanel.add(resultLabel);
+        formPanel.add(seasonResultLabel);
+        formPanel.add(resultTextLabel);
+        formPanel.add(scrollPane);
 
-    formPanel.add(countryLabel);
-    formPanel.add(countryTextField);
-    formPanel.add(monthLabel);
-    formPanel.add(monthTextField);
-    formPanel.add(cityLabel);
-    formPanel.add(cityTextField);
-    formPanel.add(timeOfDayLabel);
-    formPanel.add(timeOfDayTextField);
-    formPanel.add(temperatureLabel);
-    formPanel.add(temperatureTextField);
-    formPanel.add(resultLabel);
-    formPanel.add(seasonResultLabel);
-    formPanel.add(resultTextLabel);
-    formPanel.add(scrollPane);
+        // Create the submit button
+        JButton submitButton = new JButton("Submit");
 
-    // Create the submit button
-    JButton submitButton = new JButton("Submit");
+        // Add action listener to the submit button
+        submitButton.addActionListener(e -> {
+            String country = countryTextField.getText();
+            int month = Integer.parseInt(monthTextField.getText());
+            String city = cityTextField.getText();
+            String timeOfDay = timeOfDayTextField.getText();
+            double temperature = Double.parseDouble(temperatureTextField.getText());
 
-    // Add action listener to the submit button
-    submitButton.addActionListener(e -> {
-        String country = countryTextField.getText();
-        int month = Integer.parseInt(monthTextField.getText());
-        String city = cityTextField.getText();
-        String timeOfDay = timeOfDayTextField.getText();
-        double temperature = Double.parseDouble(temperatureTextField.getText());
+            // Find and display the corresponding season
+            String season = SeasonHandler.getSeason(seasonsData, country, month);
+            if (season != null) {
+                seasonResultLabel.setText(season);
+            } else {
+                seasonResultLabel.setText("Unknown");
+            }
 
-        // Find and display the corresponding season
-        String season = SeasonHandler.getSeason(seasonsData, country, month);
-        String result = TemperatureHandler.compareTemperature(country, city, timeOfDay, temperature);
-        System.out.println(result);
+            // Compare temperature
+            String result = TemperatureHandler.compareTemperature(country, city, timeOfDay, temperature);
+            resultTextArea.setText(result);
+        });
 
-        if (season != null) {
-            seasonResultLabel.setText("The season in " + country + " for month " + month + " is: " + season);
-        } else {
-            seasonResultLabel.setText("Season data not found for the given country and month.");
-        }
+        // Create a panel for the submit button
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(51, 51, 51));
+        buttonPanel.add(submitButton);
 
-        resultTextArea.setText(result);
-    });
+        // Add the form panel and button panel to the frame
+        scrollPane.setPreferredSize(new Dimension(300, 150)); // Adjust the width and height as needed
 
-    // Create a panel for the submit button
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setBackground(new Color(51, 51, 51));
-    buttonPanel.add(submitButton);
+        frame.add(formPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
 
-    // Add the form panel and button panel to the frame
-    scrollPane.setPreferredSize(new Dimension(300, 150)); // Adjust the width and height as needed
+        // Make the frame resizable
+        frame.setResizable(true);
 
-    frame.add(formPanel, BorderLayout.CENTER);
-    frame.add(buttonPanel, BorderLayout.SOUTH);
-
-    // Make the frame resizable
-    frame.setResizable(true);
-
-    // Make the frame visible
-    frame.setVisible(true);
-}
+        // Make the frame visible
+        frame.setVisible(true);
+    }
 }
--- TemperatureHandler.java
+++ TemperatureHandler.java
@@ -1,95 +1,42 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class TemperatureHandler {
-private static final Map<String, Map<String, Double>> temperatureData = new HashMap<>();
-public static void readTemperatureDataFromFile(String filename) {
-    try {
-        File file = new File(filename);
-        Scanner scanner = new Scanner(file);
+    private static HashMap<String, Double> temperatures;
 
-        // Skip the header line
-        scanner.nextLine();
-
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(",");
-            String country = parts[0];
-            String city = parts[1];
-            String time = parts[2];
-            double temperature = Double.parseDouble(parts[3]);
-
-            String key = city + "_" + time;
-            Map<String, Double> cityTemperatures = temperatureData.getOrDefault(country, new HashMap<>());
-            cityTemperatures.put(key, temperature);
-            temperatureData.put(country, cityTemperatures);
+    public static void readTemperatureDataFromFile(String fileName) {
+        temperatures = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                if (values.length == 4) {
+                    String key = values[0] + "," + values[1] + "," + values[2];
+                    double temperature = Double.parseDouble(values[3]);
+                    temperatures.put(key, temperature);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        scanner.close();
-    } catch (FileNotFoundException e) {
-        System.out.println("File not found: " + filename);
     }
-}
 
-public static String compareTemperature(String country, String city, String timeOfDay, double temperature) {
-    String formattedCountry = formatCountry(country);
-    String formattedCity = formatCity(city);
-    String result = "";
+    public static String compareTemperature(String country, String city, String timeOfDay, double temperature) {
+        String key = country + "," + city + "," + timeOfDay;
 
-    String key = formattedCity + "_" + timeOfDay;
-
-    Map<String, Double> cityTemperatures = temperatureData.get(formattedCountry);
-    if (cityTemperatures != null) {
-        Double averageTemperature = calculateAverage(cityTemperatures, key);
-
-        result += timeOfDay + " Average Temperature: " + averageTemperature + "\n";
-
-        if (temperature < averageTemperature) {
-            result += "The temperature is colder than the " + timeOfDay.toLowerCase() + " average.";
-        } else if (temperature > averageTemperature) {
-            result += "The temperature is warmer than the " + timeOfDay.toLowerCase() + " average.";
+        if (temperatures.containsKey(key)) {
+            double storedTemperature = temperatures.get(key);
+            if (temperature > storedTemperature) {
+                return "Warmer";
+            } else if (temperature < storedTemperature) {
+                return "Colder";
+            } else {
+                return "Same temperature";
+            }
         } else {
-            result += "The temperature is equal to the " + timeOfDay.toLowerCase() + " average.";
-        }
-    } else {
-        return "Temperature data not found for the given country and city.";
-    }
-
-    return result;
-}
-
-private static double calculateAverage(Map<String, Double> cityTemperatures, String key) {
-    double sum = 0;
-    int count = 0;
-
-    for (Map.Entry<String, Double> entry : cityTemperatures.entrySet()) {
-        String entryKey = entry.getKey();
-        if (entryKey.startsWith(key)) {
-            sum += entry.getValue();
-            count++;
+            return "Temperature data not found";
         }
     }
-
-    return count > 0 ? sum / count : 0.0;
-}
-
-private static String formatCountry(String country) {
-    return country.trim();
-}
-
-private static String formatCity(String city) {
-    String[] words = city.trim().split("\\s+");
-    StringBuilder formattedCity = new StringBuilder();
-    for (String word : words) {
-        if (!word.isEmpty()) {
-            formattedCity.append(Character.toUpperCase(word.charAt(0)))
-                    .append(word.substring(1).toLowerCase())
-                    .append(" ");
-        }
-    }
-    return formattedCity.toString().trim();
-}
 }
